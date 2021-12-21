0bdea72e4ba84f7fb984732b9df4d34e498e0353
“Marcus <marcus.saraiva@gmail.com>
2021-12-21 10:25:01 -0300

    Fixed script to calculate accessibility.

--- R/02_calculate_accessibility.R
+++ R/02_calculate_accessibility.R
@@ -9,34 +9,18 @@ library("tidyverse")
 library("tictoc")
 library("mapview")
 library("sf")
+library("data.table")
 
-
-od_zones <- st_read(here::here("data/gla/inputs", "od_zones_sg.shp"))
+od_zones <- st_read(here::here("data", "od_zones_gla.gpkg"))
 od_zones %>% mapview()
 
-od_codes <- unique(od_zones$code)
-
-od_df <- read_csv(here::here("data/gla/inputs", "wu03ew_v2_transport.csv")) %>%
-  janitor::clean_names() %>%
-  filter(area_of_workplace %in% od_codes)
-
-head(od_df)
-
-jobs_df <- od_df %>%
-  group_by(area_of_workplace) %>%
-  summarise(jobs = sum(all_categories_method_of_travel_to_work), .groups = "drop")
-
-od_points_sf <- od_zones %>%
-  left_join(jobs_df, by=c("code"="area_of_workplace")) %>%
-  st_centroid() %>%
-  st_transform(4326) %>%
-  select(id = code, jobs, geometry)
-
+od_points_sf <- st_read(here::here("data", "od_points_gla.gpkg"))
+od_points_sf %>% mapview()
 
 
 # r5r_core <- setup_r5(system.file("extdata", package = "r5r"), verbose = FALSE)
 tic()
-r5r_core <- setup_r5(here::here("data/gla"), verbose = TRUE)
+r5r_core <- setup_r5(here::here("data"), verbose = TRUE)
 toc()
 
 transit <- transit_network_to_sf(r5r_core)
@@ -47,7 +31,7 @@ transit$routes %>%
 access_df <- accessibility(r5r_core,
                            origins = od_points_sf,
                            destinations = od_points_sf,
-                           mode = c("WALK", "BUS"),
+                           mode = c("WALK", "TRANSIT"),
                            opportunities_colname = "jobs",
                            departure_datetime = as.POSIXct("14-12-2021 09:00:00", format = "%d-%m-%Y %H:%M:%S"),
                            cutoffs = c(30, 45),
@@ -57,10 +41,13 @@ access_df %>%
   left_join(od_zones, by = c("from_id"="code")) %>%
   arrange(accessibility) %>%
   ggplot(aes(fill=accessibility)) +
-  geom_sf(aes(geometry=geometry), color=NA) +
+  geom_sf(aes(geometry=geom), color=NA) +
   scale_fill_distiller(palette = "Spectral") +
   facet_wrap(~cutoff)
 
+access_df %>%
+  left_join(od_zones, by = c("from_id"="code")) %>%
+  filter(cutoff == 30) %>%
+  st_as_sf() %>%
+  mapview(zcol="accessibility")
 
-st_write(od_points_sf, here::here("data/gla/inputs/od_points_gla.gpkg"))
-st_write(od_zones, here::here("data/gla/inputs/od_zones_gla.gpkg"))
